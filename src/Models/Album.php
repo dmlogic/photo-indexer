747e0f78bf386e077156aa19134767d6bcffae5c
Darren Miller <darren.miller@ihasco.co.uk>
2021-11-22 16:28:13 +0000

    Prepare for conversion to package

---
+++ src/Models/Album.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace DmLogic\GooglePhotoIndex\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Album extends Model
+{
+    protected $table = 'albums';
+    protected $guarded = [];
+
+    public function photos()
+    {
+        return $this->hasMany('App\Models\Photo');
+    }
+}
